aacf9ba4bb1c7364b665ee0c7c2d2860356ea445
vinod.madubashana <vinod.madubashana@cloudsolution-sa.com>
2020-03-16 16:58:22 +0530

    add the basic project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TemplateDrivenFormComponent} from './template-driven-form/template-driven-form.component';
+import {HomeComponent} from './home/home.component';
+import {ReactiveFormComponent} from './reactive-form/reactive-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+  path: 'template-driven',
+  component: TemplateDrivenFormComponent
+  },
+  {
+    path: 'reactive',
+    component: ReactiveFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormComponent,
+    HomeComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
